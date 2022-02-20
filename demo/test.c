fc9930e1b95e995b1938f3e6973dd7b660de4e49
matsui54 <haru.matu9168@gmail.com>
2022-02-20 14:59:02 +0900

    Update

--- demo/test.c
+++ demo/test.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/mman.h>
 
 int foo() { return 0; }
 
 int main(int argc, char *argv[]) {
-  for (int j = 0; j < 10; j++) {
-    printf("%d\n", j);
+  // for printf
+  for (size_t i = 10; i < 200; i++) {
+    printf("%d\n", i);
   }
   return 0;
 }
